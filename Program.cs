49b3894208c56ca52f75cec43b63bdd3d3ed5a1e
Score2 <836696023@qq.com>
2022-10-26 01:36:45 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+using Tune;
+using System.Text.Json;
+using System.Net.Http.Json;
+
+namespace TuneHijacker // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            var bind = args[0];
+
+            //var bind = "94EA32ADF778@Bedroom";
+
+            /*//TuneBlade.client.PutAsync("StreamingMode", new StringContent("{\"StreamingMode\":\"RealTime\",\"BufferSize\":-1}"));
+            var a = TuneBlade.GetStreaming()!;
+            a.StreamingMode = "RealTime";
+            Console.WriteLine(JsonSerializer.Serialize(a));
+            //_ = TuneBlade.client.PutAsJsonAsync("StreamingMode", a).Result;
+            TuneBlade.client.PutAsync("StreamingMode", new StringContent(JsonSerializer.Serialize(a)));*/
+
+
+            //Console.ReadKey();
+
+            while (true)
+            {
+                try
+                {
+                    KeepTuneRunning();
+                    var stats = GetMusicStats();
+                    var device = TuneBlade.GetDevice(bind);
+                    if (stats == MusicStats.RUNNING)
+                    {
+                        var streaming = TuneBlade.GetStreaming();
+                        if (streaming.StreamingMode != "RealTime")
+                        {
+                            streaming.StreamingMode = "RealTime";
+                            TuneBlade.SetStreaming(streaming);
+                        }
+
+                        if (device.Status != "Connected")
+                        {
+                            device.connect();
+                        }
+
+                    }
+                    else
+                    {
+                        if (device.Status != "Disonnect")
+                        {
+                            device.disconnect();
+                        }
+                    }
+
+                    Console.WriteLine("current state: " + stats);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                Thread.Sleep(2000);
+            }
+
+        }
+
+        static void KeepTuneRunning()
+        {
+            if (!IsProccessRunning("TuneBlade") || !TuneBlade.IsWebActive())
+            {
+                var p = new Process(); //实例一个Process类，启动一个独立进程
+                p.StartInfo.FileName = "C:\\Program Files (x86)\\TuneBlade\\TuneBlade\\TuneBlade.exe"; //设定程序名
+                p.StartInfo.Arguments = "Silent Isolate StartHttpControl Port=54412";
+                p.StartInfo.CreateNoWindow = true; // 设置不显示窗口
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.Start();
+                Console.WriteLine("Restart TuneBlade on 54412.");
+            }
+
+
+        }
+
+
+
+
+        static bool IsProccessRunning(string name)
+        {
+            if (Process.GetProcessesByName(name).Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static MusicStats GetMusicStats()
+        {
+            if (!IsProccessRunning("WsaService"))
+            {
+                return MusicStats.STOPPED;
+            }
+            {
+                var connection = RunAdbCmd("connect localhost:58526");
+
+                if (!connection.Contains("already"))
+                {
+                    Console.WriteLine(connection);
+                }
+                
+            }
+
+
+            MusicStats stats;
+            var result = RunAdbCmd("-s localhost:58526 shell dumpsys activity services com.apple.android.music");
+            if (result.Contains("(nothing)"))
+            {
+                stats = MusicStats.STOPPED;
+            }
+            else
+            {
+                stats = MusicStats.RUNNING;
+            }
+            return stats;
+        }
+
+        static string RunAdbCmd(string arg)
+        {
+            var p = new Process(); //实例一个Process类，启动一个独立进程
+            p.StartInfo.FileName = "adb.exe"; //设定程序名
+            p.StartInfo.Arguments = arg;
+            p.StartInfo.UseShellExecute = false; //关闭Shell的使用
+            p.StartInfo.RedirectStandardInput = true; //重定向标准输入
+            p.StartInfo.RedirectStandardOutput = true; //重定向标准输出
+            p.StartInfo.RedirectStandardError = true; //重定向错误输出
+            p.StartInfo.CreateNoWindow = true; // 设置不显示窗口
+            p.StartInfo.ErrorDialog = false;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+            return p.StandardOutput.ReadToEnd();
+        }
+
+        enum MusicStats
+        {
+            STOPPED,
+            RUNNING,
+
+        }
+    }
+
+}
---
+++ Tune.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+
+namespace Tune
+{
+    public class TuneBlade
+    {
+
+        // TuneBlade web url
+        public static string url = "http://127.0.0.1:54412";
+
+        public static HttpClient client = new(new HttpClientHandler()
+        {
+            UseDefaultCredentials = true
+        })
+        {
+            Timeout = TimeSpan.FromSeconds(1),
+            BaseAddress = new Uri(url)
+        };
+
+        
+
+        public static List<TuneDevice> GetDevices()
+        {
+            return client.GetFromJsonAsync<List<TuneDevice>>("devices").Result!;
+        }
+
+        public static TuneDevice GetDevice(string ID)
+        {
+            return GetDevices().Find(x => x.ID == ID)!;
+        }
+
+
+        public static bool IsWebActive()
+        {
+            try
+            {
+                _ = client.GetAsync("devices").Result;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+            }
+
+
+            return false;
+        }
+
+        public static Streaming GetStreaming()
+        {
+            return client.GetFromJsonAsync<Streaming>("StreamingMode").Result!;
+        }
+
+        public static void SetStreaming(Streaming streaming)
+        {
+            //client.PutAsJsonAsync("StreamingMode", streaming);
+
+            client.PutAsync("StreamingMode", new StringContent(JsonSerializer.Serialize(streaming)));
+        }
+
+
+    }
+    public class Streaming
+    {
+        public string? StreamingMode { get; set; }
+        public int? BufferSize { get; set; }
+
+    }
+
+    public class TuneDevice
+    {
+
+        public string? ID { get; set; }
+        public string? Name { get; set; }
+        public int? Volume { get; set; }
+        public string? Status { get; set; }
+        public string? Substate { get; set; }
+        public bool? Buffering { get; set; }
+        public int? BufferingPercent { get; set; }
+
+        //public TuneDevice(string ID, string Name, int Volume, string Status, string Substate, bool Buffering, int BufferingPercent)
+        //{
+        //    this.ID = ID;
+        //    this.Name = Name;
+        //    this.Volume = Volume;
+        //    this.Status = Status;
+        //    this.Substate = Substate;
+        //    this.Buffering = Buffering;
+        //    this.BufferingPercent = BufferingPercent;
+        //}
+
+
+        public void connect()
+        {
+            Status = "Connect";
+
+            string[] arr = { "Status" };
+            push(arr);
+        }
+
+        public void disconnect()
+        {
+            Status = "Disconnect";
+
+            string[] arr = { "Status" };
+            push(arr);
+        }
+
+
+        public void push()
+        {
+            string[] arr = { };
+            push(arr);
+        }
+
+        public void push(string[] filter, bool ignore = false)
+        {
+            var node = JsonNode.Parse(JsonSerializer.Serialize(this))!;
+            var json = node.AsObject();
+            
+            if (ignore)
+            {
+                foreach (var i in filter)
+                {
+                    json.Remove(i);
+                }
+            }
+            else
+            {
+                // 极不优雅
+                string[] arr = { "ID", "Name", "Volume", "Status", "Substate", "Buffering", "BufferingPercent" };
+                foreach (var i in arr)
+                {
+                    if (filter.Contains(i))
+                    {
+                        continue;
+                    }
+                    json.Remove(i);
+                }
+            }
+
+
+
+            //TuneBlade.client.PutAsJsonAsync("devices/" + ID, this);
+            TuneBlade.client.PutAsync("devices/" + ID, new StringContent(json.ToJsonString()));
+            
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+    }
+}
